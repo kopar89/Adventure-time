c3a6a173f9f4578bfb5a58112b9bff4da319035f
kopar89 <158470217+kopar89@users.noreply.github.com>
2025-12-13 13:03:29 +0300

    обновленная реализация "абстрактная фабрика"

--- hero_info/persons/persons.go
+++ hero_info/persons/persons.go
@@ -1,8 +1,20 @@
 package persons
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type HeroInterface interface {
+	GetInfo() string
+	GetDamage() int
+	GetHealth() int
+	SetHealth(int)
+}
+
+type EnemyInterface interface {
+	GetInfo() string
+	GetDamage() int
+	GetHealth() int
+	SetHealth(int)
+}
 
 type Person struct {
 	Name string
@@ -18,111 +30,78 @@ type Hero struct {
 	Power   int
 }
 
+func (h *Hero) GetInfo() string {
+	return fmt.Sprintf("%s | HP: %d | Damage: %d | Mana: %d | Special: %s | Power: %d",
+		h.Name, h.Health, h.Damage, h.Mana, h.Special, h.Power)
+}
+
+func (h *Hero) GetDamage() int {
+	return h.Damage
+}
+
+func (h *Hero) GetHealth() int {
+	return h.Health
+}
+
+func (h *Hero) SetHealth(hp int) {
+	h.Health = hp
+}
+
 type Enemy struct {
-	name   string
-	damage int
-	health int
+	Name   string
+	Damage int
+	Health int
 }
 
-// Блок героев
-var Hero1 = Hero{
-	Name:    "Артур",
-	Damage:  45,
-	Mana:    35,
-	Health:  100,
-	Special: "Shoter",
-	Power:   25,
+func (e *Enemy) GetInfo() string {
+	return fmt.Sprintf("%s | HP: %d | Damage: %d", e.Name, e.Health, e.Damage)
 }
 
-var Hero2 = Hero{
-	Name:    "Джозеф",
-	Damage:  30,
-	Mana:    40,
-	Health:  120,
-	Special: "Medic",
-	Power:   17,
+func (e *Enemy) GetDamage() int {
+	return e.Damage
 }
 
-var Hero3 = Hero{
-	Name:    "Питер",
-	Damage:  36,
-	Mana:    40,
-	Health:  100,
-	Special: "Engineer",
-	Power:   20,
+func (e *Enemy) GetHealth() int {
+	return e.Health
 }
 
-var Hero4 = Hero{
-	Name:    "Карлос",
-	Damage:  25,
-	Mana:    50,
-	Health:  150,
-	Special: "Defender",
-	Power:   15,
+func (e *Enemy) SetHealth(hp int) {
+	e.Health = hp
 }
 
-// Блок злодеев
-var Enemy_1 = Enemy{
-	name:   "Гоблин",
-	damage: 20,
-	health: 100,
+type CharacterFactory interface {
+	CreateHero(heroType int) HeroInterface
+	CreateEnemy(enemyType int) EnemyInterface
 }
 
-var Enemy_2 = Enemy{
-	name:   "Лучница",
-	damage: 30,
-	health: 100,
-}
+type Factory1 struct{}
 
-// Инициализация бойцов
-type Warrior1 struct {
-	Hero
-	Power int
-}
-
-func (w Warrior1) Special_hit_hero_1(enemyHealth int) int {
-	if w.Mana > 60 {
-		fmt.Printf("%s наносит спец. удар с уроном %d!\n", w.Name, w.Power)
+func (f *Factory1) CreateHero(heroType int) HeroInterface {
+	switch heroType {
+	case 1:
+		return &Hero{Name: "Артур", Damage: 45, Mana: 35, Health: 100, Special: "Shooter", Power: 25}
+	case 2:
+		return &Hero{Name: "Джозеф", Damage: 30, Mana: 40, Health: 120, Special: "Medic", Power: 17}
+	case 3:
+		return &Hero{Name: "Питер", Damage: 36, Mana: 40, Health: 100, Special: "Engineer", Power: 20}
+	case 4:
+		return &Hero{Name: "Карлос", Damage: 25, Mana: 50, Health: 150, Special: "Defender", Power: 15}
+	default:
+		return &Hero{Name: "Артур", Damage: 45, Mana: 35, Health: 100, Special: "Shooter", Power: 25}
 	}
-	return enemyHealth - w.Power
 }
 
-type Warrior2 struct {
-	Hero
-	Power int
-}
-
-func (w Warrior2) Special_hit_hero_2(enemyHealth int) int {
-	if w.Mana > 50 {
-		fmt.Printf("%s наносит спец. удар с уроном %d!\n", w.Name, w.Power)
+func (f *Factory1) CreateEnemy(enemyType int) EnemyInterface {
+	switch enemyType {
+	case 1:
+		return &Enemy{Name: "Гоблин", Damage: 20, Health: 100}
+	case 2:
+		return &Enemy{Name: "Лучница", Damage: 30, Health: 100}
+	case 3:
+		return &Enemy{Name: "Орк", Damage: 25, Health: 120}
+	case 4:
+		return &Enemy{Name: "Волшебник", Damage: 35, Health: 80}
+	default:
+		return &Enemy{Name: "Гоблин", Damage: 20, Health: 100}
 	}
-	return enemyHealth - w.Power
-}
-
-type Warrior3 struct {
-	Hero
-	Power int
-}
-
-func (w Warrior3) Special_hit_hero_3(enemyHealth int) int {
-	if w.Mana > 50 {
-		fmt.Printf("%s наносит спец. удар с уроном %d!\n", w.Name, w.Power)
-	}
-	return enemyHealth - w.Power
-}
-
-type Warrior4 struct {
-	Hero
-	Power int
-}
-
-func (w Warrior4) Special_hit_hero_4(enemyHealth int) int {
-	if w.Mana > 40 {
-		fmt.Printf("%s наносит спец. удар с уроном %d!\n", w.Name, w.Power)
-	}
-	return enemyHealth - w.Power
-}
-
-func (h Hero) Info() string {
-	return fmt.Sprintf("%s | Урон: %d | Мана: %d | Здоровье: %d", h.Name, h.Damage, h.Mana, h.Health)
 }
